885348253f49e733e29de637fac12bfa8ef9c6e9
ashokkanap <ashokjava92@gmail.com>
2020-01-20 14:52:11 -0500

    cleanup the code.

--- src/main/java/com/spring/integration/IntegrationConfig.java
+++ src/main/java/com/spring/integration/IntegrationConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlowBuilder;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.core.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
